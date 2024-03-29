7201c0b52b13c070e39e1706c192b28eaa44f8f8
Rachelle <rrosiles@hawk.iit.edu>
2024-03-29 18:52:28 -0500

    working solutions

---
+++ 1D Schrodinger Equation Solution Dif Form/Potential.swift
@@ -0,0 +1,8 @@
+//
+//  Potential.swift
+//  1D Schrodinger Equation Solution Dif Form
+//
+//  Created by PHYS 440 Rachelle on 3/29/24.
+//
+
+import Foundation
---
+++ 1D Schrodinger Equation Solution Dif Form/Runge-Kutta 4.swift
@@ -0,0 +1,8 @@
+//
+//  Runge-Kutta 4.swift
+//  1D Schrodinger Equation Solution Dif Form
+//
+//  Created by PHYS 440 Rachelle on 3/22/24.
+//
+
+import Foundation
